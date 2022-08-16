ed1815c0dfa5a63332aeb9679c6b55c4cf5ee1a7
joy <joy@gmail.com>
2022-08-16 22:53:46 +0800

    Optimize time zone

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 git clone https://github.com/iDerekLi/juejin-helper && cd juejin-helper/workflows && yarn
 apt-get update
 apt-get install cron
-echo "5 18 * * * cd /var/dev/juejin-helper/workflows && yarn checkin" >> /var/spool/cron/crontabs/root
-echo "15 18 * * * cd /var/dev/juejin-helper/workflows && yarn seagold" >> /var/spool/cron/crontabs/root
+echo "5 7 * * * cd /var/dev/juejin-helper/workflows && yarn checkin" >> /var/spool/cron/crontabs/root
+echo "15 7 * * * cd /var/dev/juejin-helper/workflows && yarn seagold" >> /var/spool/cron/crontabs/root
 service cron start
 
 tail -f >/dev/null
